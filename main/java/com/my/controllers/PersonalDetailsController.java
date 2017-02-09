e4778f77188d84efcbe8a966a5e215eaecae1666
crao <chetan.nagarajrao@gmail.com>
2017-02-08 21:21:56 -0600

    Clean imports

--- main/java/com/my/controllers/PersonalDetailsController.java
+++ main/java/com/my/controllers/PersonalDetailsController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import com.my.binding.PersonalDetailsModel;
 import com.my.binding.PreferencesBinding;
 import com.my.model.Member;
