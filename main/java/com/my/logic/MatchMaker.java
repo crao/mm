21f2fc554f1b1e13bc38c45483300f34163f95b1
crao <chetan.nagarajrao@gmail.com>
2017-03-08 16:31:04 -0600

    MatchMaker and Score intial Classes

---
+++ main/java/com/my/logic/MatchMaker.java
@@ -0,0 +1,14 @@
+package com.my.logic;
+
+import com.my.model.Member;
+import com.my.model.Score;
+
+public class MatchMaker {
+	
+	public Score makeMatch(Member m1, Member m2){
+		
+		return null;
+		
+	}
+
+}
