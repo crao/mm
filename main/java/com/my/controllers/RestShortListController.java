4fcc08436231ca7d558506f6d1cefd781a5de3f7
crao <chetan.nagarajrao@gmail.com>
2017-03-17 23:01:30 -0500

    shortlist changes

--- main/java/com/my/controllers/RestShortListController.java
+++ main/java/com/my/controllers/RestShortListController.java
@@ -2,6 +2,8 @@ package com.my.controllers;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,13 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class RestShortListController {
 	
 	@GetMapping("/shortList")
-	public ResponseEntity<?> shortList(@RequestParam(value = "slId", required = true) long slId,
-			@RequestParam(value = "userId", required = true) long userId,HttpSession session){
+	public ResponseEntity<?> shortList(@RequestParam(value = "slId", required = false) String slId,
+			@RequestParam(value = "userId", required = false) String userId,HttpSession session){
 		
 		System.out.println("#*#*#*#*#*#*#*#*#");
 		System.out.println("USER ID: "+userId+" SlId: "+slId);
 		System.out.println("#*#*#*#*#*#*#*#*#");
-		return null;
+		return new ResponseEntity("Successfully", new HttpHeaders(), HttpStatus.OK);
 	}
 
 }
--- main/resources/static/js/main.js
+++ main/resources/static/js/main.js
@@ -11,6 +11,8 @@ $(document).ready(function () {
 
 });
 
+
+
 function fire_ajax_submit() {
 
     // Get form
@@ -53,5 +55,88 @@ function fire_ajax_submit() {
 
         }
     });
-
 }
+    
+    $("slist").click(function() {
+    	alert("In jquery 2");
+	})
+    
+  //  $('a[title=sl]').click(function (event){ 
+$('a[title=sl]').click(function (event){
+    	debugger;
+     alert("In jquery");
+       event.preventDefault(); 
+       $.ajax({
+           url: $(this).attr('href')
+          ,success: function(response) {
+        	   $("#result1").text(response);
+               console.log("SUCCESS : ", response);
+               
+           },
+           error: function (e) {
+
+               $("#result1").text(e.responseText);
+               console.log("ERROR : ", e);
+               console.log("ERROR : ", data);
+               $("#btnSl").prop("disabled", false);
+
+           }
+        })
+        return false; //for good measure
+   });
+    
+    
+    $(document).ready(function () {
+
+        $("#btnSl").click(function (event) {
+
+            //stop submit the form, we will post it manually.
+            event.preventDefault();
+
+            fire_sl_submit();
+
+        });
+
+    });
+    
+    function fire_sl_submit() {
+    	debugger;
+        // Get form
+        var form = $('#sl')[0];
+
+        var data = new FormData(form);
+        var uid = $("#uid");
+        var sid = $("#sid");
+      //  data.append("CustomField", "This is some extra data, testing");
+
+        $("#btnSl").prop("disabled", true);
+
+        $.ajax({
+            type: "GET",       
+            url: "/shortList?"+"userId="+uid+"&slId="+sid,
+            data: data,
+            //http://api.jquery.com/jQuery.ajax/
+            //https://developer.mozilla.org/en-US/docs/Web/API/FormData/Using_FormData_Objects
+           processData: false, //prevent jQuery from automatically transforming the data into a query string
+            contentType: false,
+            cache: false,
+            timeout: 600000,
+        
+            success: function (data) {
+
+                $("#result1").text(data);
+                console.log("SUCCESS : ", data);
+                $("#btnSl").prop("disabled", false);
+
+            },
+            error: function (e) {
+
+                $("#result1").text(e.responseText);
+                console.log("ERROR : ", e);
+                console.log("ERROR : ", data);
+                $("#btnSl").prop("disabled", false);
+
+            }
+        });
+    }
+
