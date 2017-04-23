ca8260157204f223824eb5c182debc30851df412
crao <chetan.nagarajrao@gmail.com>
2017-04-22 23:42:29 -0500

    messages service

---
+++ main/java/com/my/service/MessageService.java
@@ -0,0 +1,16 @@
+package com.my.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.my.model.Message;
+
+@Service
+public class MessageService {
+	
+	private List<Message> getMessagesTo(long memId){
+		return null;
+	}
+
+}
