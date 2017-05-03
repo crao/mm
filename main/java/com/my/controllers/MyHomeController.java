de9bdc559ad3357db63d0503071d6cf9cf9d146b
crao <chetan.nagarajrao@gmail.com>
2017-05-02 20:42:14 -0500

    myhome

--- main/java/com/my/controllers/MyHomeController.java
+++ main/java/com/my/controllers/MyHomeController.java
@@ -2,6 +2,7 @@ package com.my.controllers;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,13 +10,24 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.my.model.Member;
+import com.my.model.Photo;
+import com.my.service.PhotoService;
 @Controller
 public class MyHomeController {
+	
+	@Autowired
+	private PhotoService photoService;
+	
 	@RequestMapping(value="/myhome",method=RequestMethod.GET)
 	public String start(Model model,@RequestParam(value="status",required=false)String status,
 			@RequestParam(value="userId",required=false)String userId,HttpSession session){
-				Member member = (Member)session.getAttribute("member");
-				model.addAttribute("member", member);
+			Member member = (Member)session.getAttribute("member");
+			model.addAttribute("member", member);
+			
+			Photo profilePhoto = photoService.getProfilePhoto(Long.parseLong(userId));
+			model.addAttribute("profilePhoto", profilePhoto.getFileName());
+				
+				
 		return "myhome";
 		
 	}
